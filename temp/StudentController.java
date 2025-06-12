758e599442431ce82db66c5c1f2753156141a2f6
PrabuddhaRShrestha <pshrestha@pmsquare.com>
2025-06-12 06:12:57 +0000

    Initial commit from Django

---
+++ temp/StudentController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    // Define controller methods based on the PHP logic
+
+}
---
+++ temp/StudentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    // Implement service methods based on the PHP logic
+
+}
